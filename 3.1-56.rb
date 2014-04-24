b0f66c872dcd73fbb704104ef05f8afabcc2bbde
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2014-04-24 18:17:42 +0900

    update anything

---
+++ 3.1-56.rb
@@ -0,0 +1,42 @@
+# encode:utf-8
+
+def main
+  report = Report.new
+  report.output_report(:plain)
+end
+
+class Report
+  def initialize
+    @title = '月次報告'
+    @text = [ '順調', '最高の調子' ]
+  end
+
+  def output_report(format)
+    if format == :plain
+      puts("*** #{@title} ***")
+    else
+      puts('<html>')
+      puts('  <head>')
+      puts("    <title>#{@title}</title>")
+      puts('  </head>')
+      puts('  <body>')
+    end
+
+    @text.each do |line|
+      if format == :plain
+        puts(line)
+      else
+        puts("    <p>#{line}</p>")
+      end
+    end
+
+    if format == :html
+      puts('  </body>')
+      puts('</html>')
+    end
+  end
+end
+
+if __FILE__ == $0
+  main
+end
