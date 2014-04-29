52382cfe22c540354ab1388fbc933093da09051d
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2014-04-29 23:44:20 +0900

    add 3.4-6[12]*.rb

---
+++ 3.4-61.1.rb
@@ -0,0 +1,39 @@
+#encoding:utf-8
+require './3.4-61.rb'
+
+def main
+  report = HTMLReport.new
+  report.output_report
+end
+
+class HTMLReport < Report
+  def output_start
+    puts('<html>')
+  end
+
+  def output_head
+    puts('  <head>')
+    puts("    <title>#{@title}</title>")
+    puts('  </head>')
+  end
+
+  def output_body_start
+    puts('  <body>')
+  end
+
+  def output_line(line)
+    puts("    <p>#{line}</p>")
+  end
+
+  def output_body_end
+    puts('  </body>')
+  end
+
+  def output_end
+    puts('</html>')
+  end
+end
+
+if __FILE__ == $0
+  main
+end
---
+++ 3.4-61.2.rb
@@ -0,0 +1,33 @@
+#encoding:utf-8
+require './3.2-57.rb'
+
+def main
+  report = PlainTextReport.new
+  report.output_report
+end
+
+class PlainTextReport < Report
+  def output_start
+  end
+
+  def output_head
+    puts("****#{@title}****")
+  end
+
+  def output_body_start
+  end
+
+  def output_line(line)
+    puts(line)
+  end
+
+  def output_body_end
+  end
+
+  def output_end
+  end
+end
+
+if __FILE__ == $0
+  main
+end
