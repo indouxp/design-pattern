0555a1d6a89b5dd6364df6ad6b7fc5e9b17b3229
Tatsuo Indou <tatsuo-i@mtb.biglobe.ne.jp>
2014-05-05 20:41:45 +0900

    update 4.4-p79

---
+++ 4.4-p79.1.rb
@@ -0,0 +1,12 @@
+#encoding: utf-8
+
+def run_it
+  puts("Before the yield")
+  yield
+  puts("After the yield")
+end
+
+run_it do
+  puts("Hello")
+  puts("Coming to you from inside the blocks")
+end
--- 4.4-p79.rb
+++ 4.4-p79.rb
@@ -5,3 +5,12 @@ n = multiply.call(20, 3)
 puts n
 n = multiply.call(10, 50)
 puts n
+
+multiply = proc do |x, y|
+  x * y
+end
+
+n = multiply.call(20, 3)
+puts n
+n = multiply.call(10, 50)
+puts n
